d5b77690e427fa0da7e2e6f134b0d7524b467c65
Dehoux <nick-brouwer@hotmail.com>
2014-06-17 19:51:48 +0200

    Last init

---
+++ ShuppetHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShuppetServiceLibrary;
+using System.ServiceModel;
+
+namespace ShuppetHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(ShuppetService)))
+            {
+                host.Open();
+                Console.WriteLine("Service ready");
+                Console.ReadKey();
+            }
+        }
+    }
+}
